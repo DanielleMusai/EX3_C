ddf832554d5d1270c6a4ec8daa09f4de6fe77cbc
DanielleMusai <92378800+DanielleMusai@users.noreply.github.com>
2021-12-19 22:39:36 +0200

    Update and rename string.h to stringProglib.h

---
+++ stringProglib.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "stringProglib.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include <stddef.h>
+#define TXT 1024
+#define WORD 30
+
+int checkLetter(char c);// check the value of each char
+int num(char *st);//a word numro
+int isSign(char c);// check if its only letters
+void Gematria(char *word, char *text);// A
+int Atbash1(char a);// check the atbash for a single char
+char *reverse(char *c);// a word reverse
+char *atbashWord(char *word);//translate the word to atbash
+void Atbash(char *word, char *text);//B
+int charForC(char c);// check if there is a unnecessary sign
+void Anagram(char *word, char *text);// C
